d53c00a94361e1753916ca57e8e71223b7f5d31d
Akulij <aakulij@gmail.com>
2025-03-30 18:17:03 +0800

    use bot token from config

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
     dotenvy::dotenv()?;
     let config = Config::init_from_env()?;
 
-    let bot = Bot::from_env();
+    let bot = Bot::new(config.bot_token);
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
         bot.send_dice(msg.chat.id).await?;
