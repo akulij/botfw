0d0225741fb878f6daa0eb2ccb36187fb9551be6
Akulij <aakulij@gmail.com>
2025-03-30 18:12:58 +0800

    init dotenv

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use teloxide::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    dotenvy::dotenv()?;
+
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
