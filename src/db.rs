b2a27e3330424ea3cc6b51b5b41965890ceb6534
Akulij <aakulij@gmail.com>
2025-04-03 23:16:28 +0900

    delete comments of code :/

--- src/db.rs
+++ src/db.rs
@@ -5,8 +5,6 @@ use crate::Config;
 use self::models::*;
 
 use diesel::prelude::*;
-//use diesel::query_dsl::methods::FilterDsl;
-//use diesel::{prelude::*, r2d2::{ConnectionManager, Pool}};
 use diesel_async::AsyncPgConnection;
 use diesel_async::RunQueryDsl;
 use diesel_async::pooled_connection::AsyncDieselConnectionManager;
@@ -14,7 +12,6 @@ use diesel_async::pooled_connection::bb8::Pool;
 
 #[derive(Clone)]
 pub struct DB {
-    //pool: Pool<ConnectionManager<AsyncPgConnection>>
     pool: diesel_async::pooled_connection::bb8::Pool<AsyncPgConnection>,
 }
 
@@ -22,18 +19,12 @@ impl DB {
     pub async fn new<S: Into<String>>(db_url: S) -> Self {
         let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(db_url);
         let pool = Pool::builder().build(config).await.unwrap();
-        //let mg = diesel::r2d2::ConnectionManager::new(db_url);
-        //let pool = diesel::r2d2::Pool::builder()
-        //    .max_size(15)
-        //    .build(mg)
-        //    .unwrap();
         DB { pool }
     }
 
     pub async fn get_users(&mut self) -> Vec<User> {
         use self::schema::users::dsl::*;
         let mut conn = self.pool.get().await.unwrap();
-        //let mut conn = AsyncPgConnection::establish(&std::env::var("DATABASE_URL").unwrap()).await.unwrap();
         users
             .filter(id.gt(0))
             .load::<User>(&mut conn)
@@ -44,8 +35,6 @@ impl DB {
     pub async fn set_admin(&mut self, userid: i64, isadmin: bool) {
         use self::schema::users::dsl::*;
         let connection = &mut self.pool.get().await.unwrap();
-        //diesel::update(users).filter(id.eq(userid)).set(is_admin.eq(true)).execute(connection);
-        //diesel::update(users).filter(id.eq(userid)).set(is_admin.eq(true)).load(connection).await.unwrap();
         diesel::update(users)
             .filter(id.eq(userid))
             .set(is_admin.eq(isadmin))
